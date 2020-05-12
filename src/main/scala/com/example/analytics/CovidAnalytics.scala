aaf55259c7d430ed12d04f11c3bbbee25ad687d0
Jayant Chauhan <jayantjagdishchauhan@gmail.com>
2020-05-12 10:35:09 +0530

    Updated DF format

--- src/main/scala/com/example/analytics/CovidAnalytics.scala
+++ src/main/scala/com/example/analytics/CovidAnalytics.scala
@@ -15,8 +15,8 @@ object CovidAnalytics {
   val covidData: String = AppUtil.covidData
 
   def main(args: Array[String]): Unit = {
-    Logger.getLogger("org").setLevel(Level.ERROR)
-    Logger.getLogger("akka").setLevel(Level.ERROR)
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
 
     val data: List[Data] = objectMapper.readValue(covidData, classOf[Array[Data]]).toList
 
@@ -26,27 +26,28 @@ object CovidAnalytics {
 
     import sparkSession.implicits._
     val regionDF: DataFrame = sparkContext.parallelize[RegionData](regionData).toDF()
-    val newDF = regionDF.na.drop().na.fill(0)
+    val regionWiseTotalCasesDF = regionDF.na.drop().na.fill(0)
       .withColumn("totalInfected", regionDF("totalInfected").cast(IntegerType))
       .withColumn("recovered", regionDF("recovered").cast(IntegerType))
       .withColumn("deceased", regionDF("deceased").cast(IntegerType))
       .groupBy("region").sum("totalInfected", "recovered", "deceased")
+      .withColumnRenamed("sum(totalInfected)", "totalInfected")
+      .withColumnRenamed("sum(recovered)", "recovered")
+      .withColumnRenamed("sum(deceased)", "deceased")
       .sort(asc("region"))
 
-    newDF.printSchema()
-    newDF.show(1000)
+    regionWiseTotalCasesDF.printSchema()
+    regionWiseTotalCasesDF.show(1000)
 
     val withRegionDF: DataFrame = sparkContext.parallelize[Data](filteredDataWithRegion).toDF().na.drop()
     val withoutRegionDF: DataFrame = sparkContext.parallelize[Data](filteredDataWithoutRegion).toDF().na.drop()
     val wholeDF = withRegionDF.drop("regionData", "sourceUrl", "readMe")
       .union(withoutRegionDF.drop("regionData", "sourceUrl", "readMe"))
       .orderBy(asc("lastUpdatedAtApify"))
-    wholeDF.show()
-    regionDF.show()
-    println(regionDF.count())
+    val modifiedDF = wholeDF.withColumn("lastUpdatedAtApify", date_format(wholeDF("lastUpdatedAtApify"), "dd/MM/YYYY"))
 
     //Highest number of deaths top 10
-    wholeDF.sort(desc("deaths")).show(10)
+    modifiedDF.sort(desc("lastUpdatedAtApify")).show(100)
 
   }
 }
